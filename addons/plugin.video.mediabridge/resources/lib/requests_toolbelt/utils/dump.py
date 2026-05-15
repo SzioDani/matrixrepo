f75517109e7859a1e6fec4e4c717eb5346678b5d
SzioDani <mario.matranga@libero.it>
2026-05-16 00:47:41 +0200

    Add files via upload

---
+++ addons/plugin.video.mediabridge/resources/lib/requests_toolbelt/utils/dump.py
@@ -0,0 +1,5 @@
+# Stub per requests_toolbelt.utils.dump - usato solo per debug in cloudscraper
+def dump_response(response, request_prefix=b'< ', response_prefix=b'> '):
+    return b''
+def dump_all(response, request_prefix=b'< ', response_prefix=b'> '):
+    return b''
